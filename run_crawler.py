6d82e6cfd2e8879ed62f910f193d9d38e8448267
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2017-02-22 20:48:06 +0800

    add debug flag

--- run_crawler.py
+++ run_crawler.py
@@ -1,6 +1,6 @@
 from crawler import crawler
 
-debug = True
+debug = __debug__
 host = "0.0.0.0"
 
 crawler.app.run(host=host, port=9527, debug=debug)
--- run_newsfeed.py
+++ run_newsfeed.py
@@ -1,6 +1,6 @@
 from newsfeed import newsfeed
 
-debug = True
+debug = __debug__
 host = "0.0.0.0"
 
 newsfeed.app.run(host=host, port=9528, debug=debug)

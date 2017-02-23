0dfd17e7468733544d9f09423b2fca5afdf91928
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2017-02-24 03:17:13 +0800

    set default fields of feed

--- fbfeed/filter.py
+++ fbfeed/filter.py
@@ -9,18 +9,20 @@ class FbFeedFilter:
 
     def __init__(self, fbid, num, include_text='', search=False):
         self.fbid = fbid
-        self.num = int(num)
         self.include_text = include_text
+        self.search = search
+        self.num = int(num)
         self.graph = fb_init()
         self.tzinfo = "Asia/Taipei"
-        self.search = search
+        self.fields = ["id", "type", "description", "message", "created_time", "updated_time", "from", "permalink_url", "name"]
 
     def _download(self, encoding='utf-8'):
         items = []
+        fields = ",".join(self.fields)
         group = load_group(self.graph, self.fbid, search=self.search)
         if group:
             items = load_pages(self.graph, self.fbid, "feed", self.num,
-                               search=self.search, date_format="U")
+                               search=self.search, date_format="U", fields=fields)
         return self._data_prepare(items)
 
     def _data_prepare(self, items):
@@ -33,7 +35,6 @@ class FbFeedFilter:
         items = data_cleaner("message", items)
         items = data_cleaner("story", items)
         items = data_inserter(self.include_text, "keyword", items)
-        items = data_inserter(self.fbid, "fbid", items)
         return items
 
     def output(self):

2d35fd37f1d8e789a1220ead6a271bda744e8f41
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2017-02-23 10:18:08 +0800

    refactoring

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -13,6 +13,5 @@ async def index(request, methods=['GET']):
     url = request.args.get('url')
     if not url:
         return json({'crawler'})
-
     obj = fetchNews(url)
     return json(obj, ensure_ascii=False)

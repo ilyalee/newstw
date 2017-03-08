775ba2f7ab942e22fe9bfaf1b3fb73f7ae9abd8b
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2017-03-09 01:39:43 +0800

    set REQUEST_TIMEOUT

--- archiver/news_archive.py
+++ archiver/news_archive.py
@@ -7,6 +7,9 @@ from archiver.blueprints import bp_v1
 from sanic.response import html
 from db.providers.archive_provider import ArchiveProvider
 from jinja2 import Environment, PackageLoader, select_autoescape
+from sanic.config import Config
+
+Config.REQUEST_TIMEOUT = 300
 env = Environment(
     loader=PackageLoader('archiver', 'templates'),
     autoescape=select_autoescape(['html'])

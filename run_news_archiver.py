9023069c80c6bdf1a801211c26e03bf16f011713
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2017-03-09 17:10:21 +0800

    set workers=1 to avoid requests problems

--- run_news_archiver.py
+++ run_news_archiver.py
@@ -5,4 +5,5 @@ debug = __debug__
 host = "0.0.0.0"
 port = 9530
 
-news_archive.app.run(host=host, port=port, debug=debug, workers=os.cpu_count())
+# workaround: set workers to 1
+news_archive.app.run(host=host, port=port, debug=debug, workers=1)

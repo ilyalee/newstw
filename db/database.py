bb6d979cd45121648d5dafd179f1f833057039cd
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2017-03-09 17:06:27 +0800

    add missing uri parameter

--- db/database.py
+++ db/database.py
@@ -15,7 +15,7 @@ sqlite_mode = db_url.startswith('sqlite://')
 
 if sqlite_mode:
     import sqlite3
-    creator = lambda: sqlite3.connect('file::memory:?cache=shared')
+    creator = lambda: sqlite3.connect('file::memory:?cache=shared', uri=True)
 
 if creator:
     engine = create_engine('sqlite://', creator=creator)

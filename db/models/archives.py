cfef47ecb129ee3fdd0e9638c57fd7905ce9a756
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2017-03-22 04:04:40 +0800

    use bigint

--- db/models/archives.py
+++ db/models/archives.py
@@ -1,21 +1,21 @@
 from sqlalchemy import (
-    Column, String, Integer, DateTime, Text
+    Column, String, BigInteger, DateTime, Text
 )
 from sqlalchemy.schema import Index
 from db.models import Base
 from arrow import Arrow
 
+
 class Archive(Base):
     __tablename__ = 'archives'
 
-    id = Column(Integer, autoincrement=True, primary_key=True)
+    id = Column(BigInteger, autoincrement=True, primary_key=True)
     hash = Column(String, nullable=False, unique=True)
     published = Column(DateTime(timezone=True), nullable=False)
     title = Column(String, nullable=False)
     summary = Column(Text, nullable=False)
     link = Column(String, nullable=False)
     source = Column(String, nullable=False)
-
 
     def __repr__(self):
         return '<Archive(title="{}")>'.format(self.title)

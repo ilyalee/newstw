3ef63d73e001f69356dfddbb9c0b53f65133858e
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2017-03-22 07:25:23 +0800

    fix id issue

--- db/models/archives.py
+++ db/models/archives.py
@@ -1,5 +1,5 @@
 from sqlalchemy import (
-    Column, String, BigInteger, DateTime, Text
+    Column, String, Integer, DateTime, Text
 )
 from sqlalchemy.schema import Index
 from db.models import Base
@@ -8,13 +8,14 @@ from db.models import Base
 class Archive(Base):
     __tablename__ = 'archives'
 
-    id = Column(BigInteger, autoincrement=True, primary_key=True)
+    id = Column(Integer, autoincrement=True, primary_key=True)
     hash = Column(String, nullable=False, unique=True)
     published = Column(DateTime(timezone=True), nullable=False)
     title = Column(String, nullable=False)
     summary = Column(Text, nullable=False)
     link = Column(String, nullable=False)
     source = Column(String, nullable=False)
+    founds = Column(String, nullable=True)
 
     def __repr__(self):
         return '<Archive(title="{}")>'.format(self.title)

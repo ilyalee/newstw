2b74d11d9f21029bb104d44a2fb3462e11dee664
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2017-03-22 02:24:09 +0800

    introduce new scheduler

--- scripts/scheduler.py
+++ scripts/scheduler.py
@@ -1,19 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+import sys
+sys.path.append(os.getcwd())
 
-from apscheduler.schedulers.background import BlockingScheduler
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from archiver.controllers.newsfeed import archive_feed_by_filter
+from utils.data_utils import keyword_builder
+from multiprocessing import Pool
+import asyncio
+import configparser
+config = configparser.ConfigParser()
+config.read('scripts/feeds.cfg')
+sources_pm = config.items("Print media")
+sources_em = config.items("Electronic media")
+keywords = config.get("Feed filter", "keywords")
 
-
-#TODO: add real jobs
-def run():
-    print('Tick! The time is: %s' % datetime.now())
+async def news_observer():
+    include_text = keyword_builder(keywords)
+    for name, url in sources_pm:
+        print("Get feed from [{}] ...".format(name))
+        data = await archive_feed_by_filter(url, include_text)
+        print("Job [{}] is done. {}".format(name, data["info"]))
+    print("All done.")
 
 if __name__ == '__main__':
-    scheduler = BlockingScheduler()
-    scheduler.add_executor('processpool')
-    scheduler.add_job(run, 'interval', hours=1)
+    scheduler = AsyncIOScheduler()
+    # every hours
+    scheduler.add_job(news_observer, 'cron', minute=38)
+
+    scheduler.start()
+    print("News Observer started.")
+    #print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
 
     try:
-        scheduler.start()
+        asyncio.get_event_loop().run_forever()
     except (KeyboardInterrupt, SystemExit):
-        scheduler.shutdown()
+        pass

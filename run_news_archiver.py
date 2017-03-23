3526ef8daf9525226d87af4e5b48e1ba16027335
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2017-03-24 01:50:55 +0800

    set workers

--- run_news_archiver.py
+++ run_news_archiver.py
@@ -1,8 +1,8 @@
 from archiver import news_archive
-
+import os
 debug = __debug__
 host = "0.0.0.0"
 port = 9530
 
 # workaround: set workers to 1
-news_archive.app.run(host=host, port=port, debug=debug, workers=1)
+news_archive.app.run(host=host, port=port, debug=debug, workers=os.cpu_count())

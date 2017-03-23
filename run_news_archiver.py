98d2d3baaee870b5271246902d61d5d8a597a370
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2017-03-24 01:48:02 +0800

    add debug info

--- run_news_archiver.py
+++ run_news_archiver.py
@@ -3,6 +3,7 @@ import os
 debug = __debug__
 host = "0.0.0.0"
 port = 9530
-
+if debug:
+    print("os.cpu_count()", os.cpu_count())
 # workaround: set workers to 1
 news_archive.app.run(host=host, port=port, debug=debug, workers=os.cpu_count())

1d4779b9817217cd75535b4592531f03becf752f
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2017-03-27 21:30:26 +0800

    refactor

--- archiver/utils/archiver_helper.py
+++ archiver/utils/archiver_helper.py
@@ -8,13 +8,9 @@ ap = ArchiveProvider()
 
 
 def fetch_report(page, limit, keywords, category):
-    sources = None
-    if category:
-        sources = load_sources_by_category(category)
-    return ap.load_report_by_page(page, limit, keyword, sources)
+    sources = load_sources_by_category(category)
+    return [] if category and not sources else ap.load_report_by_page(page, limit, keyword, sources)
 
 async def as_fetch_report(page, limit, keywords, category):
-    sources = None
-    if category:
-        sources = load_sources_by_category(category)
-    return await ap.as_load_report_by_page(page, limit, keywords, sources)
+    sources = load_sources_by_category(category)
+    return [] if category and not sources else await ap.as_load_report_by_page(page, limit, keywords, sources)
--- archiver/utils/archiver_utils.py
+++ archiver/utils/archiver_utils.py
@@ -8,6 +8,6 @@ def load_sources_by_category(category):
     config.read('config/feeds.cfg')
     sources_pm, _ = zip(*config.items("Print media"))
     sources_em, _ = zip(*config.items("Electronic media"))
-    sources_cat = lambda category: [category] if category in set(
-        sources_pm) or category in set(sources_em) else []
-    return {'pmedia': sources_pm, 'emedia': sources_em}.get(category, sources_cat(category))
+    sources = lambda category: [category] if category in set(
+        sources_pm) or category in set(sources_em) else None
+    return {'pmedia': sources_pm, 'emedia': sources_em}.get(category, sources(category))

0a5e407492efc1ea72d673675b63c1df39d415d3
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2017-03-28 00:31:10 +0800

    add executors

--- scripts/scheduler.py
+++ scripts/scheduler.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import asyncio
+import os
 from observer import news_observer
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
@@ -10,9 +11,12 @@ if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal.SIG_DFL)
 
     scheduler = AsyncIOScheduler()
+    executors = {
+        'default': ThreadPoolExecutor(5),
+        'processpool': ProcessPoolExecutor(os.cpu_count())
+    }
     # every hours
-    scheduler.add_job(news_observer, 'cron', minute=0, timezone="Asia/Taipei")
-
+    scheduler.add_job(news_observer, 'cron', minute=0, timezone="Asia/Taipei", executors=executors)
     scheduler.start()
     print("News Observer started.")
     # print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))

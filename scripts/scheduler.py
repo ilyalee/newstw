af39c2deafefca1c0564a3fc1abf2b9c4156538d
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2017-03-28 01:08:16 +0800

    fix typo

--- scripts/scheduler.py
+++ scripts/scheduler.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import asyncio
 import os
+import sys
+sys.path.append(os.getcwd())
+
 import settings
+import asyncio
 from observer import news_observer
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
 
 if __name__ == '__main__':
     import signal
     signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-    scheduler = AsyncIOScheduler()
     executors = {
         'default': ThreadPoolExecutor(settings.LIMIT),
         'processpool': ProcessPoolExecutor(os.cpu_count())
     }
+    scheduler = AsyncIOScheduler(executors=executors)
     # every hours
-    scheduler.add_job(news_observer, 'cron', minute=0, timezone="Asia/Taipei", executors=executors)
+    scheduler.add_job(news_observer, 'cron', minute=0, timezone="Asia/Taipei")
     scheduler.start()
     print("News Observer started.")
     # print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))

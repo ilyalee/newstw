3cd62fee6f97c719c15a53b3d40da80c8e5ed386
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2017-03-28 01:36:12 +0800

    add retry mode

--- crawler/utils/crawler_helper.py
+++ crawler/utils/crawler_helper.py
@@ -38,7 +38,7 @@ async def as_fetch_news(url, encoding='utf-8', timeout=60):
             return await NewsDataProcessor(resp.url, html).as_output()
 
 
-def fetch_news_all(urls, encoding='utf-8', timeout=60, limit=5):
+def fetch_news_all(urls, encoding='utf-8', timeout=60, limit=5, remedy=False):
     from concurrent.futures import ThreadPoolExecutor
     from requests_futures.sessions import FuturesSession
     import threading
@@ -47,20 +47,27 @@ def fetch_news_all(urls, encoding='utf-8', timeout=60, limit=5):
     resopones = []
     with FuturesSession(session=requests.Session(),
                         executor=ThreadPoolExecutor(max_workers=os.cpu_count())) as session:
+        failed_urls = []
         futures = ((url, session.get(url, timeout=timeout)) for url in urls)
         for url, future in futures:
+            if remedy:
+                log.info(f"[{__name__}] Retry: {url}")
             try:
                 with sem:
                     resopones.append(future.result())
             except requests.exceptions.RequestException as e:
+                failed_urls.append(url)
                 log.error(f"[{__name__}] Failure when trying to fetch {url}")
                 log.info(e, exc_info=True)
                 continue
-
         for resp in resopones:
             resp.encoding = encoding
             html = clean_html(resp.text)
             news = NewsDataProcessor(resp.url, html)
             output = news.output()
             collect.append(output)
-    return collect
+
+    if failed_urls:
+        return collect + fetch_news_all(failed_urls, encoding, timeout, limit, True)
+    else:
+        return collect

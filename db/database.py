a19e76129fbdc25a4dd3a057b7754ccd05da1198
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2017-03-29 21:51:40 +0800

    change pool size

--- db/database.py
+++ db/database.py
@@ -26,7 +26,7 @@ else:
         engine = create_engine(db_url, connect_args={
                                'check_same_thread': False})
     else:
-        engine = create_engine(db_url, pool_size=50)
+        engine = create_engine(db_url, pool_size=1)
 
 if sqlite_memory_mode:
     from db.models import Base

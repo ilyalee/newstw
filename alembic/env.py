303a1049a3b431e1dedc27da23bd2ac99cadca0d
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2017-04-07 13:55:23 +0800

    cleanup

--- alembic/env.py
+++ alembic/env.py
@@ -2,7 +2,8 @@ from __future__ import with_statement
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
-import os, sys
+import os
+import sys
 sys.path.append(os.getcwd())
 
 import settings

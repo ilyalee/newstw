edb4af776e72d8a6967b316f3ea97249d5f5e3fb
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2017-04-07 23:01:42 +0800

    add facebook_observer

--- scripts/scheduler.py
+++ scripts/scheduler.py
@@ -9,6 +9,7 @@ sys.path.append(os.getcwd())
 import settings
 import asyncio
 from observer import news_observer
+from observer_facebook import facebook_observer
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
 import setproctitle
@@ -22,6 +23,7 @@ if __name__ == '__main__':
     scheduler = AsyncIOScheduler()
     # every hours
     scheduler.add_job(news_observer, 'cron', minute=0, timezone="Asia/Taipei")
+    scheduler.add_job(facebook_observer, 'cron', minute=30, timezone="Asia/Taipei")
     scheduler.start()
     print("News Observer started.")
     # scheduler.print_jobs()

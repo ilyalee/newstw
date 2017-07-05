01a4cff1a200cbe75d3affbdc6da57b39a9606b8
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2017-07-06 05:46:56 +0800

    add trimtext

--- crawler/web_shape_var.py
+++ crawler/web_shape_var.py
@@ -171,12 +171,19 @@ context['mirrormedia'] = [
 skip['any'] = []
 
 trimtext['any'] = []
-trimtext['ltn'] = ['相關影音']
+trimtext['ltn'] = ['相關影音', '相關新聞']
 trimtext['ctitv'] = ['※中天快點TV非讀不可:']
 trimtext['udn'] = ['分享facebook']
 trimtext['appledaily'] = [
     '有話要說 投稿「即時論壇」',
     '想了解更多中部房市訊息，請到蘋果地產王粉絲團',
     '【加入環境資訊中心粉絲頁，掌握環境新知！】',
-    '【更多新聞，請看《蘋果陪審團》粉絲團】'
+    '【更多新聞，請看《蘋果陪審團》粉絲團】',
+    '想知道更多旅遊去處，請參加蘋果花愛旅遊社團',
+    '社團「蘋果花愛Shopping」',
+    '【推薦新聞】',
+    '延伸閱讀：'
+]
+trimtext['ebc'] = [
+    '【今日最熱門】'
 ]
